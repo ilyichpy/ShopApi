c0d3e9abf58ad7e4311018654863b1a5697f70fa
ilyichpy <ilyaszuev25@gmail.com>
2024-02-24 23:38:08 +0300

    crud for product

--- src/main/java/com/example/shopapi/controller/ProductController.java
+++ src/main/java/com/example/shopapi/controller/ProductController.java
@@ -1,20 +1,15 @@
 package com.example.shopapi.controller;
 
-import com.example.shopapi.model.Client;
 import com.example.shopapi.model.Product;
-import com.example.shopapi.model.Supplier;
 import com.example.shopapi.response.ProductResponse;
-import com.example.shopapi.service.ClientService;
 import com.example.shopapi.service.ImageService;
 import com.example.shopapi.service.ProductService;
 import com.example.shopapi.service.SupplierService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -31,7 +26,7 @@ public class ProductController {
 			if (!supplierService.saveSupplier(product.getSupplier())) {
 				return ProductResponse.builder()
 						.message("Not enough info for supplier")
-						.key(501)
+						.key(404)
 						.product(product)
 						.build();
 			}
@@ -40,7 +35,7 @@ public class ProductController {
 			if (!imageService.saveImageInDB(product.getImageID())) {
 				return ProductResponse.builder()
 						.message("Fill image info")
-						.key(501)
+						.key(404)
 						.product(product)
 						.build();
 			}
@@ -51,7 +46,7 @@ public class ProductController {
 			return ProductResponse.builder()
 					.message("Some empty fields in product")
 					.product(product)
-					.key(501)
+					.key(404)
 					.build();
 		}
 		return ProductResponse.builder()
@@ -59,6 +54,36 @@ public class ProductController {
 				.product(product)
 				.key(200)
 				.build();
+	}
+
+	@PutMapping("changeStock")
+	public ProductResponse changeAvailableStock(@RequestBody Product product) {
+		if(!productService.changeAvailableStock(product, product.getAvailableStock())) {
+			return ProductResponse.builder()
+					.message("invalid stock value")
+					.key(404)
+					.product(null)
+					.build();
+		}
+		return ProductResponse.builder()
+				.message("Data successfully changed")
+				.key(200)
+				.product(productService.findClientById(product.getId()))
+				.build();
+	}
+
+	@GetMapping("findById")
+	public Product findById(@RequestBody Product product) {
+		return productService.findClientById(product.getId());
+	}
+
+	@GetMapping("allProducts")
+	public List<Product> findAllProducts() {
+		return productService.findAllProducts();
+	}
+
+	public void deleteProduct(@RequestBody Product product) {
+		productService.deleteProductById(product.getId());
 	}
 
 }
--- src/main/java/com/example/shopapi/service/ProductService.java
+++ src/main/java/com/example/shopapi/service/ProductService.java
@@ -5,11 +5,18 @@ import com.example.shopapi.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
 	private final ProductRepository productRepository;
+
+	public Product findClientById(UUID id) {
+		return productRepository.findById(id).get();
+	}
 
 	public boolean saveProductInDB(Product product) {
 		if (product.notEnoughInfoForProduct()) {
@@ -17,5 +24,22 @@ public class ProductService {
 		}
 		productRepository.save(product);
 		return true;
+	}
+
+	public boolean changeAvailableStock(Product product, long valueNew) {
+		if (valueNew < 0L) {
+			return false;
+		}
+		product.setAvailableStock(valueNew);
+		productRepository.save(product);
+		return true;
+	}
+
+	public List<Product> findAllProducts() {
+		return productRepository.findAll();
+	}
+
+	public void deleteProductById(UUID id) {
+		productRepository.deleteById(id);
 	}
 }
